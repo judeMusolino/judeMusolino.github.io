23bb59e07a7b3227619e45bf7fb6ac4d11d3a789
judeMusolino <jnm5704@psu.edu>
2025-09-04 17:06:02 -0400

    Update my-proj.js
    modal overlay

--- src/my-proj.js
+++ src/my-proj.js
@@ -57,7 +57,7 @@ export class MyProj extends LitElement {
         display: flex;
         position: fixed;
         top: 0; left: 0; right: 0; bottom: 0;
-        background: rgba(0,0,0,0.6);
+        background: #13111199;
         justify-content: center;
         align-items: center;
         z-index: 1000;
